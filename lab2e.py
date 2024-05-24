0cc1cb7f7454391a3b06e873ae949ea21f10b1a8
Sagar K C <sk-c13@myseneca.ca>
2024-05-24 14:43:07 -0400

    Added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Author: Sagar K C
+# Author ID: sk-c13
+# Date Created: 2024/05/23
+
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+
+print("blast off!")
